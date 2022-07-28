f824f2ae721d9d85cf127253906e760c158b43af
Robert Carr <70494103+RobertCarrUTA@users.noreply.github.com>
2022-07-27 19:52:58 -0500

    Adding a TODO

--- die.cpp
+++ die.cpp
@@ -8,6 +8,7 @@ Die::Die(int faces, int value, int roll) : _faces{faces}, _value{value}
 {
     if (faces == 0) // If you have 0 faces then you don't have dice to roll.
     {
+        // TODO: Come back and fix this, if a user enters 0 faces, the program will crash.
         return;
     }
     else
